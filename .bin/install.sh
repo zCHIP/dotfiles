667f2a4c47d2c11d3d4cc12720929f5a526ba32a
Gennady Temnikov <zerochip@gmail.com>
2021-01-03 18:03:13 -0800

    Add setup script

---
+++ .bin/install.sh
@@ -0,0 +1,17 @@
+# This script sets up the '.dotfiles' repo
+# Original: https://www.atlassian.com/git/tutorials/dotfiles
+
+git clone --bare git@github.com:zCHIP/dotfiles.git $HOME/.dotfiles
+function config {
+   git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME $@
+}
+mkdir -p .dotfiles-backup
+config checkout
+if [ $? = 0 ]; then
+  echo "Checked out config.";
+  else
+    echo "Backing up pre-existing dot files.";
+    config checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} .dotfiles-backup/{}
+fi;
+config checkout
+config config status.showUntrackedFiles no
